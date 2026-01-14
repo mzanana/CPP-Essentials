34bef71aff651bf5d84835ad7870726a11b81117
mzanana <mr.zananamohamed@gmail.com>
2026-01-14 18:50:16 +0100

    This is my first cpp implementation working the megaphone exercice which based on uppercasing all the arguments

---
+++ 1337_Projects/CPP00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+
+int main (int ac, char **av)
+{
+	int i;
+	int j;
+
+	i = 1;
+	if(ac == i)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (av[i])
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				std::cout << (char)toupper(av[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
