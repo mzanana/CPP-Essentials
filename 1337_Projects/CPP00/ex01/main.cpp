15a146a1479d8c14d6b11cd92ba476cdeca8b495
mzanana <mr.zananamohamed@gmail.com>
2026-01-16 12:29:18 +0100

    Create the main.cpp function

---
+++ 1337_Projects/CPP00/ex01/main.cpp
@@ -0,0 +1,27 @@
+#include "PhoneBook.hpp"
+
+int main()
+{
+    PhoneBook   book;
+    std::string command;
+
+    while (true)
+    {
+        std::cout << "Enter a command (ADD, SEARCH, or EXIT): ";
+        
+        if (!std::getline(std::cin, command))
+        {
+            std::cout << "\n...EOF" << std::endl;
+            break;
+        }
+        if (command == "ADD")
+			book.AddContact();
+        // else if (command == "SEARCH")
+		// 	book.SearchContacts();
+        else if (command == "EXIT")
+            break;
+        else if (!command.empty())
+            continue;
+    }
+    return (0);
+}
