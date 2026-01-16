9771a515ed7c32593dba54e999890f6c879e8307
mzanana <mr.zananamohamed@gmail.com>
2026-01-16 12:04:43 +0100

    edit the phonebook header file

--- 1337_Projects/CPP00/ex01/PhoneBook.hpp
+++ 1337_Projects/CPP00/ex01/PhoneBook.hpp
@@ -7,8 +7,9 @@ class PhoneBook
 {
     private:
         Contact contacts[8];
-        int nbSaved;
+        int nbr;
         int index;
+		std::string ReadInput(std::string prompt, int mode);
 
     public:
         PhoneBook();
