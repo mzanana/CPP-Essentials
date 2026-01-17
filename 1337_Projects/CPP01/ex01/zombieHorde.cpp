9fec9ca52646ef3da1c62fe2f7b3b54ce458e636
mzanana <mr.zananamohamed@gmail.com>
2026-01-17 16:48:48 +0100

    delete the files of the previous exercice and create the zombieHorde

--- 1337_Projects/CPP01/ex01/zombieHorde.cpp
+++ 1337_Projects/CPP01/ex01/zombieHorde.cpp
@@ -1,12 +1,13 @@
 #include "Zombie.hpp"
 
-Zombie* newZombie(std::string name)
-{
-    Zombie *ret = new Zombie(name);
-    return (ret);
-}
 Zombie *zombieHorde(int N, std::string name)
 {
+    int i = 0;
     Zombie *zombies = new Zombie[N];
+    while (i < N)
+    {
+        zombies[i].setName(name);
+        i++;
+    }
     return (zombies);
 }
