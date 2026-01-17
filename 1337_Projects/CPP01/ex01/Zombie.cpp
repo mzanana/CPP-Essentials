16221d3a7fa0a3a144fe62715d7a1bbbadf3f611
mzanana <mr.zananamohamed@gmail.com>
2026-01-17 16:49:36 +0100

    remove the useless functions of the previous exercice

--- 1337_Projects/CPP01/ex01/Zombie.cpp
+++ 1337_Projects/CPP01/ex01/Zombie.cpp
@@ -1,15 +1,16 @@
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name) : name(name){}
+Zombie::Zombie(){}
 
-void Zombie::announce(void)
-{
-    std::cout << this->name << ": BraiiiiiiinnnzzzZ...\n";
-}
 
 std::string Zombie::getName(void)
 {
     return (this->name);
+}
+
+void Zombie::announce(void)
+{
+    std::cout << getName() << ": BraiiiiiiinnnzzzZ...\n";
 }
 
 Zombie::~Zombie()
--- 1337_Projects/CPP01/ex01/Zombie.hpp
+++ 1337_Projects/CPP01/ex01/Zombie.hpp
@@ -16,7 +16,5 @@ class Zombie
         ~Zombie();
 };
 
-Zombie* newZombie(std::string name);
-void randomChump(std::string name);
 Zombie *zombieHorde(int N, std::string name);
 #endif
