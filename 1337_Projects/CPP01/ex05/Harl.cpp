bb73e7437dbb74d0d480e765e9787abb45bb5d52
mzanana <mr.zananamohamed@gmail.com>
2026-01-19 19:28:51 +0100

    change the ' implementation

--- 1337_Projects/CPP01/ex05/Harl.cpp
+++ 1337_Projects/CPP01/ex05/Harl.cpp
@@ -7,12 +7,12 @@ void Harl:: debug(void)
 
 void Harl:: info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn\\’t put enough bacon in my burger! If you did, I wouldn\\’t be asking for more!\n\n";
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n\n";
 }
 
 void Harl:: warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I\\’ve been coming for years, whereas you started working here just last month.\n\n";
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n\n";
 }
 
 void Harl::error(void)
