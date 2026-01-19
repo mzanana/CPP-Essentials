dbc254b9e2d59d9e8757cb916f78dc53420504df
mzanana <mr.zananamohamed@gmail.com>
2026-01-19 19:20:02 +0100

    the implementation of the Harl class

---
+++ 1337_Projects/CPP01/ex05/Harl.cpp
@@ -0,0 +1,37 @@
+#include "Harl.hpp"
+
+void Harl:: debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n\n";
+}
+
+void Harl:: info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn\\’t put enough bacon in my burger! If you did, I wouldn\\’t be asking for more!\n\n";
+}
+
+void Harl:: warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I\\’ve been coming for years, whereas you started working here just last month.\n\n";
+}
+
+void Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now \n\n";
+}
+void Harl:: complain(std::string input)
+{
+	std::string level[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	harlComp com[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int i = 0;
+	while (i < 4)
+	{
+		if (input == level[i])
+		{
+			(this->*com[i])();
+			return;
+		}
+		i++;
+	}
+	std::cout << "level NOT found!\n";
+}
