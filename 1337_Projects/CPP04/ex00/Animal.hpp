c692d415e803eb1f3b98bae4cc0563e527716617
mzanana <mr.zananamohamed@gmail.com>
2026-01-23 20:06:24 +0100

    Create the class animal

---
+++ 1337_Projects/CPP04/ex00/Animal.hpp
@@ -0,0 +1,22 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <string>
+#include <iostream>
+
+class Animal
+{
+	protected:
+		std::string type;
+	public:
+		Animal();
+		Animal(const Animal& other);
+		Animal& operator=(const Animal& right);
+		~Animal();
+		Animal(const std::string& type);
+		void makeSound(void) const;
+		std::string getType(void) const;
+
+};
+
+#endif
