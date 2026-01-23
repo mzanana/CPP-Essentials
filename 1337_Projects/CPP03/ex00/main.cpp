7779353f70c1e9b7c13318590196439031c9c3a6
mzanana <mr.zananamohamed@gmail.com>
2026-01-23 12:18:55 +0100

    create a main function that test all the cases

--- 1337_Projects/CPP03/ex00/main.cpp
+++ 1337_Projects/CPP03/ex00/main.cpp
@@ -1,7 +1,24 @@
 #include "ClapTrap.hpp"
-
-
 int main()
 {
-	
+    ClapTrap kibe("Kibe");
+    ClapTrap freg("Freg");
+
+    kibe.attack("Freg");
+    freg.takeDamage(0);
+
+    kibe.takeDamage(5);
+    kibe.beRepaired(3);
+
+    for (int i = 0; i < 10; i++)
+        freg.attack("target");
+    
+    freg.attack("target");
+    freg.beRepaired(5);
+
+    kibe.takeDamage(20);
+    kibe.attack("Freg");
+    kibe.beRepaired(5);
+
+    return 0;
 }
