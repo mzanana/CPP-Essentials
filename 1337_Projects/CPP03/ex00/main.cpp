6a7d76fa05049a68d4297545ef886b0fd836a420
mzanana <mr.zananamohamed@gmail.com>
2026-01-23 15:36:19 +0100

    simple main

--- 1337_Projects/CPP03/ex00/main.cpp
+++ 1337_Projects/CPP03/ex00/main.cpp
@@ -4,21 +4,14 @@ int main()
     ClapTrap kibe("Kibe");
     ClapTrap freg("Freg");
 
+    std::cout << std::endl;
     kibe.attack("Freg");
-    freg.takeDamage(0);
 
-    kibe.takeDamage(5);
-    kibe.beRepaired(3);
+    std::cout << std::endl;
+    freg.takeDamage(10);
 
-    for (int i = 0; i < 10; i++)
-        freg.attack("target");
-    
-    freg.attack("target");
-    freg.beRepaired(5);
-
-    kibe.takeDamage(20);
-    kibe.attack("Freg");
-    kibe.beRepaired(5);
+    std::cout << std::endl;
+    freg.beRepaired(10);
 
     return 0;
 }
