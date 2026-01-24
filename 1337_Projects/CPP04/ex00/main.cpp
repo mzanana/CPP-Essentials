86121722f51ef584ef803c0c33f228862bc6d781
mzanana <mr.zananamohamed@gmail.com>
2026-01-24 10:36:58 +0100

    create a main that contain both the animal and wronganimal

--- 1337_Projects/CPP04/ex00/main.cpp
+++ 1337_Projects/CPP04/ex00/main.cpp
@@ -1,19 +1,42 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
-const Animal* meta = new Animal();
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-std::cout << j->getType() << " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
 
-delete meta;
-delete i;
-delete j;
+	std::cout << std::endl << std::endl;
+	std::cout << j->getType() << std::endl;
+	std::cout << i->getType();
 
+
+	std::cout << std::endl << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	std::cout << std::endl << std::endl;
+	delete meta;
+	delete i;
+	delete j;
+
+
+
+
+	// const WrongAnimal* meta = new WrongAnimal();
+	// const WrongAnimal* i = new WrongCat();
+	// std::cout << std::endl << std::endl;
+	// std::cout << i->getType();
+
+
+	// std::cout << std::endl << std::endl;
+	// i->makeSound(); //will output the cat sound!
+	// meta->makeSound();
+
+	// std::cout << std::endl << std::endl;
+	// delete meta;
+	// delete i;
 }
