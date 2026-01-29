0c7504f57ffb2db84df36860be359ecd4c598cd1
mzanana <mr.zananamohamed@gmail.com>
2026-01-29 20:22:39 +0100

    the class declaration with the nested classes of the exceptions and the orthodox canonical form

---
+++ 1337_Projects/5-Exception Handling/ex00/Bureaucrat.hpp
@@ -0,0 +1,42 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class Bureaucrat
+{
+	private:
+		const std::string name;
+		int grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& right);
+		~Bureaucrat();
+
+		Bureaucrat(const std::string& name, int grade);
+
+		const std::string& getName(void) const;
+		int getGrade(void) const;
+
+		void incGrade(void);
+		void decGrade(void);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what()  const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream& right, const Bureaucrat& bur);
+
+#endif
