0c0c5696666ff12848d5e8834b43634cddf8f7b0
mzanana <mr.zananamohamed@gmail.com>
2026-01-31 09:51:31 +0100

    implementation of the form class

---
+++ 1337_Projects/5-Exception Handling/ex01/Form.cpp
@@ -0,0 +1,47 @@
+#include "Form.hpp"
+
+Form::Form() : name("Default Form"), isSigned(false), gradeToSign(75), gradeToExec(75){}
+
+Form::Form(const std::string& name, const int gradeToSign, const int gradeToExec) : name(name), gradeToSign(gradeToSign), gradeToExec(gradeToExec) {}
+
+Form::Form(const Form& other) : name(other.name), isSigned(other.isSigned), gradeToSign(other.gradeToSign), gradeToExec(other.gradeToExec){}
+
+Form& Form::operator=(const Form& right)
+{
+	if (this != &right)
+		this->isSigned = right.isSigned;
+	return (*this);
+}
+
+Form::~Form(){}
+
+
+const std::string Form::getName(void)
+{
+	return (this->name);
+}
+
+bool Form::getIsSigned(void)
+{
+	return (this->isSigned);
+}
+
+int Form::getGradeToSign(void)
+{
+	return (this->gradeToSign);
+}
+
+int Form::getGradeToExec(void)
+{
+	return (this->gradeToExec);
+}
+
+const char *Form::GradeTooHighException::what()  const throw()
+{
+	return ("Grade Too High!\n");
+}
+
+const char *Form::GradeTooLowException::what() const throw()
+{
+	return ("Grade Too Low!\n");
+}
