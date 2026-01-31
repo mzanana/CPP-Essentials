19341168b6d1d8897ba4ff03d2817f4b1f43dc65
mzanana <mr.zananamohamed@gmail.com>
2026-01-31 08:45:39 +0100

    change the overload insertion into the requested one into the subject

--- 1337_Projects/5-Exception Handling/ex00/Bureaucrat.cpp
+++ 1337_Projects/5-Exception Handling/ex00/Bureaucrat.cpp
@@ -60,6 +60,6 @@ const char *Bureaucrat::GradeTooLowException::what() const throw()
 
 std::ostream& operator<<(std::ostream& right, const Bureaucrat& bur)
 {
-	right << bur.getName() << " is a Bureaucrat with a grade of " << bur.getGrade();
+	right << bur.getName() << ", bureaucrat grade " << bur.getGrade() << std::endl;
 	return (right);	
 }
--- 1337_Projects/5-Exception Handling/ex00/main.cpp
+++ 1337_Projects/5-Exception Handling/ex00/main.cpp
@@ -4,33 +4,33 @@ int main()
 {
 	{	// Normal output 
 		Bureaucrat bur("simo", 3);
-		std::cout << "Before : " << bur << std::endl;
+		std::cout << "Before : " << bur;
 		bur.decGrade();
-		std::cout << "After  : " << bur << std::endl << std::endl;
+		std::cout << "After  : " << bur;
 	}
 	
 	{  //Grade Too Low Exception
+		std::cout << std::endl;	
 		try
 		{	
 			Bureaucrat bur("simo", 151);
-			std::cout << "Before : " << bur << std::endl;
+			std::cout << "Before : " << bur;
 			bur.decGrade();
-			std::cout << "After  : " << bur << std::endl;
+			std::cout << "After  : " << bur;
 		}
 		catch(Bureaucrat::GradeTooLowException& e)
 		{
 			std::cout << e.what();
 		}
 	}
-
+	
 	{  //Grade Too High Exception
 		try
 		{
-			std::cout << std::endl;	
 			Bureaucrat bur("simo", 0);
-			std::cout << "Before : " << bur << std::endl;
+			std::cout << "Before : " << bur;
 			bur.decGrade();
-			std::cout << "After  : " << bur << std::endl;
+			std::cout << "After  : " << bur;
 		}
 		catch(Bureaucrat::GradeTooHighException& e)
 		{
