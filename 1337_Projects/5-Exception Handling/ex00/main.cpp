119cfde29ddffcf64218c588f480577dac1b6eae
mzanana <mr.zananamohamed@gmail.com>
2026-01-31 09:48:29 +0100

    optimize the main function

--- 1337_Projects/5-Exception Handling/ex00/main.cpp
+++ 1337_Projects/5-Exception Handling/ex00/main.cpp
@@ -2,39 +2,32 @@
 
 int main()
 {
-	{	// Normal output 
-		Bureaucrat bur("simo", 3);
-		std::cout << "Before : " << bur;
-		bur.decGrade();
-		std::cout << "After  : " << bur;
-	}
+	try
+	{
+		{	// Normal grade 
+			Bureaucrat bur("simo", 3);
+			std::cout << "Before : " << bur;
+			bur.decGrade();
+			std::cout << "After  : " << bur;
+		}
 	
-	{  //Grade Too Low Exception
-		std::cout << std::endl;	
-		try
-		{	
+		{	// Lower grade
 			Bureaucrat bur("simo", 151);
 			std::cout << "Before : " << bur;
 			bur.decGrade();
 			std::cout << "After  : " << bur;
 		}
-		catch(Bureaucrat::GradeTooLowException& e)
-		{
-			std::cout << e.what();
-		}
-	}
-	
-	{  //Grade Too High Exception
-		try
-		{
+
+		{	// Higher grade
 			Bureaucrat bur("simo", 0);
 			std::cout << "Before : " << bur;
 			bur.decGrade();
 			std::cout << "After  : " << bur;
 		}
-		catch(Bureaucrat::GradeTooHighException& e)
-		{
-			std::cout << e.what();
-		}
+			
+	}
+	catch(std::exception& e)
+	{
+		std::cout << e.what();
 	}
 }
