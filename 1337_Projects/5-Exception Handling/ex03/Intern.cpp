dc1bc03beda065296945750d3f524dba0556236d
mzanana <mr.zananamohamed@gmail.com>
2026-02-01 22:10:39 +0100

    implement the logic of the makeForm method

--- 1337_Projects/5-Exception Handling/ex03/Intern.cpp
+++ 1337_Projects/5-Exception Handling/ex03/Intern.cpp
@@ -2,10 +2,14 @@
 
 Intern::Intern(){}
 
-Intern::Intern(const Intern& other){}
+Intern::Intern(const Intern& other)
+{
+	(void)other;
+}
 
 Intern& Intern::operator=(const Intern& right)
 {
+	(void)right;
 	return (*this);
 }
 
@@ -28,5 +32,30 @@ AForm* Intern::makePresidential(std::string target)
 
 AForm* Intern::makeForm(std::string formName, std::string target)
 {
+	int i;
+
+	std::string forms[] = {
+		"ShrubberyCreation",
+		"RobotomyRequest",
+		"PresidentialPardon"
+	};
+
+	AForm*(Intern::*retFun[])(std::string) = {
+		&Intern::makeShrubbery,
+		&Intern::makeRobotomy,
+		&Intern::makePresidential
+	};
+	i = 0;
+	while (i < 3)
+	{
+		if (formName == forms[i])
+		{
+			std::cout << "Intern creates " << formName << std::endl;
+			return (this->*retFun[i])(target);
+		}
+		i++;
 	
+	}
+	std::cout << "Intern cannot find " << formName << " form on the desk!!\n";
+	return (NULL);
 }
