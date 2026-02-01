761c423dd560f820910016ddcd9489d7aedbd122
mzanana <mr.zananamohamed@gmail.com>
2026-02-01 21:08:32 +0100

    execute() methode implementation with the executeform

---
+++ 1337_Projects/5-Exception Handling/ex02/Bureaucrat.cpp
@@ -0,0 +1,91 @@
+#include "AForm.hpp" 
+
+Bureaucrat::Bureaucrat(): name("NoName"), grade(150){}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name), grade(grade)
+{
+	if (grade < 1)
+		throw(GradeTooHighException());
+	if (grade > 150)
+		throw(GradeTooLowException());
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other): name(other.name)
+{
+	*this = other;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& right)
+{
+	if (this != &right)
+		this->grade = right.grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat(){}
+
+int Bureaucrat:: getGrade(void) const
+{
+	return (grade);
+}
+
+const std::string& Bureaucrat::getName(void) const
+{
+	return (name);
+}
+
+void Bureaucrat::incGrade()
+{
+	if (grade == 1)
+		throw (GradeTooHighException());
+	grade--;
+}
+
+void Bureaucrat::decGrade()
+{
+	if (grade == 150)
+			throw GradeTooLowException();
+	grade++;
+}
+
+const char *Bureaucrat::GradeTooHighException::what()  const throw()
+{
+	return ("Grade Too High!\n");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade Too Low!\n");
+}
+
+std::ostream& operator<<(std::ostream& right, const Bureaucrat& bur)
+{
+	right << bur.getName() << ", bureaucrat grade " << bur.getGrade() << std::endl;
+	return (right);	
+}
+
+void Bureaucrat::signForm(AForm& form)
+{
+	try
+	{
+		form.beSigned(*this);
+		std::cout << name << " sign " << form.getName() << std::endl;
+	}
+	catch(std::exception& e)
+	{
+		std::cout << name << " couldn't sign " << form.getName() << " because " << e.what();
+	}
+}
+
+void Bureaucrat::executeForm(AForm const& form) const
+{
+	try
+	{
+		form.execute(*this);
+		std::cout << name << " execute" << form.getName() << std::endl;
+	}
+	catch(std::exception& e)
+	{
+		std::cout << name << " couldn't execute " << form.getName() << " because " << e.what();
+	}
+}
---
+++ 1337_Projects/5-Exception Handling/ex02/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <cstdlib>
+#include <ctime>
+
+class AForm;
+
+class Bureaucrat
+{
+	private:
+		const std::string name;
+		int grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& right);
+		~Bureaucrat();
+
+		Bureaucrat(const std::string& name, int grade);
+
+		const std::string& getName(void) const;
+		int getGrade(void) const;
+
+		void incGrade(void);
+		void decGrade(void);
+
+		void signForm(AForm& form);
+		void executeForm(AForm const& form) const;
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what()  const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream& right, const Bureaucrat& bur);
+
+#endif
