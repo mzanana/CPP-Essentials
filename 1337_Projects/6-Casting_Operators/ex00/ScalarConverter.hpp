d7e30a4746a979a4973a95ae5abdce3d898b07d3
mzanana <mr.zananamohamed@gmail.com>
2026-02-10 20:30:15 +0100

    header file of the exercise

---
+++ 1337_Projects/6-Casting_Operators/ex00/ScalarConverter.hpp
@@ -0,0 +1,33 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <limits>
+#include <cmath>
+// #include <cctype>
+// #include <iomanip>
+
+enum Type
+{
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE,
+	OTHER
+};
+
+class ScalarConverter
+{
+	private:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter& other);
+		ScalarConverter& operator=(const ScalarConverter& right);
+		~ScalarConverter();
+
+	public:
+		static void convert(std::string input);
+};
+
+#endif
