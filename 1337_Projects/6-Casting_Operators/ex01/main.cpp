4dc4c7b4f04cb17c1e162a29edcf6977b31a3e9e
ZANANA Mohamed <mr.zananamohamed@gmail.com>
2026-02-17 17:08:56 +0000

    a simple test

--- 1337_Projects/6-Casting_Operators/ex01/main.cpp
+++ 1337_Projects/6-Casting_Operators/ex01/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include "Serializer.hpp"
 
 int main()
-
 {
-    char x = 48;
-    std::cout << &x << std::endl;
-    int* p = reinterpret_cast<int*>(&x);
-    std::cout << *p << std::endl;
-    std::cout << p << std::endl;
+    Data data = {"mzanana", 25};
+    std::cout << "Data address: " << &data << std::endl;
+    std::cout << "Age: " << data.age << std::endl;
+    std::cout << "Name: " << data.name << std::endl << std::endl;;
+    
+    uintptr_t raw = Serializer::serialize(&data);
+    std::cout << "Raw: " << raw << std::endl;
+    
+    Data* ptr = Serializer::deserialize(raw);
+    std::cout << "Data ptr address: " << ptr << std::endl;
+
 
     return 0;
 }
