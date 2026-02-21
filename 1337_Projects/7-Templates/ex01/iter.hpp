90b4547c15389e41c00ac9a339d5f7496b5ea504
mzanana <mr.zananamohamed@gmail.com>
2026-02-21 12:39:58 +0000

    the implementation of the iter template

---
+++ 1337_Projects/7-Templates/ex01/iter.hpp
@@ -0,0 +1,16 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstddef>
+
+template <typename T, typename F>
+void iter(T* array, const size_t len, F fun)
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		fun(array[i]);
+	}
+}
+
+
+#endif
