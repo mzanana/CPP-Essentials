84f715783854071def4fd783667f2107ca2c324e
mzanana <mr.zananamohamed@gmail.com>
2026-02-21 12:40:29 +0000

    test to use the iter template

---
+++ 1337_Projects/7-Templates/ex01/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "iter.hpp"
+
+template <typename T>
+void printElement(const T& element)
+{
+	std::cout << element << ' ';
+}
+
+template <typename T>
+void appElement(T& element)
+{
+	element += element;
+}
+
+int main()
+{
+	int arrayInt[5] = {1, 3, 7, 42, 0};
+	std::string arrayStr[3] = {"first", "second", "third"};
+	std::cout << "Before : ";
+	iter(arrayInt, 5, printElement<int>);
+	iter(arrayInt, 5, appElement<int>);
+	std::cout << std::endl << "After  : ";
+	iter(arrayInt, 5, printElement<int>);
+	std::cout << std::endl << "********************" << std::endl << "Before : ";
+	iter(arrayStr, 3, printElement<std::string>);
+	iter(arrayStr, 3, appElement<std::string>);
+	std::cout << std::endl << "After  : ";
+	iter(arrayStr, 3, printElement<std::string>);
+}
