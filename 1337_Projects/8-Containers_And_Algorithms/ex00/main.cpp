d383955ad88f01a3cc78623f850a47ad8ff1b235
mzanana <mr.zananamohamed@gmail.com>
2026-02-24 14:12:38 +0000

    implementation of a basic test that contain a valid and non-valid test

---
+++ 1337_Projects/8-Containers_And_Algorithms/ex00/main.cpp
@@ -0,0 +1,24 @@
+#include "easyfind.hpp"
+int main()
+{
+	std::vector<int> vec(1);
+
+	vec.push_back(2);
+	vec.push_back(5);
+	vec.push_back(-9);
+	vec.push_back(0);
+	vec.push_back(1);
+	
+	try
+	{
+		std::vector<int>::iterator it = easyfind(vec, -9);
+		std::cout << *it << std::endl;
+		it = easyfind(vec, 1337);
+		std::cout << *it << std::endl;
+	}
+	catch(std::exception& e)
+	{
+		std::cout << e.what();
+	}
+	std::cout << "******************\nThe Program Exit Successfully!\n";
+}
