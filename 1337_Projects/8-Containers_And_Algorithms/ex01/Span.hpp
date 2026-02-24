1982486ef2ea9685bd9f450477d847824a314495
mzanana <mr.zananamohamed@gmail.com>
2026-02-24 17:25:32 +0000

    implementation of the span exercise

---
+++ 1337_Projects/8-Containers_And_Algorithms/ex01/Span.hpp
@@ -0,0 +1,41 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+typedef std::vector<int>::iterator iter;
+
+class Span
+{
+	private:
+		unsigned int		N;
+		std::vector<int>	vec;
+	public:
+		Span();
+		Span(unsigned int size);
+		Span(const Span& other);
+		Span& operator=(const Span& right);
+		~Span();
+		
+		class AddErrorException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+		class NoSpanException : public std::exception
+		{
+			public:
+				const char*what() const throw();
+		};
+
+		void addNumber(int toAdd);
+		int shortestSpan();
+		int longestSpan();
+		void multipleAdd(const std::vector<int>& numbers);
+		void ft_print();
+};
+
+#endif
